2341ab377c1524f02e766ec93eb1d9bff681117c
wwwbijay <link2bj@gmail.com>
2022-01-07 11:42:44 +0545

    profile page updated

---
+++ src/app/account/IUserDetails.ts
@@ -0,0 +1,12 @@
+export interface IUserDetails {
+    fullName: string;
+    userName: string;
+    address: string;
+    email: string;
+    roles: string;
+    phoneNumber: string;
+    gender: string;
+    dateOfBirth: string;
+    department: string;
+  }
+  
